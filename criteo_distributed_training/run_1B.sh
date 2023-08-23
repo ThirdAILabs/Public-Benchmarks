ed180659f6b58c3baa204246e922a8622f9afa02
Pratik Pranav <pratik@thirdai.com>
2023-08-24 01:12:02 +0530

    add bash file to run 1B param model

---
+++ criteo_distributed_training/run_1B.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# If there aren't any command line arguments or if the argument count is not equal to 1, display an error message and exit
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <activation-key>"
+    exit 1
+fi
+
+ACTIVATION_KEY=$1
+
+# Run the script for a specific configuration (number of nodes and embedding dimensions)
+run_script() {
+  NUM_NODES=$1
+  EMBEDDING_DIMENSION=$2
+  MODEL_SIZE=$3
+
+  # Display the current configuration
+  echo "Running with ${NUM_NODES} nodes, embedding dimension ${EMBEDDING_DIMENSION}, and ${MODEL_SIZE} model size" 
+
+  # Execute the script with the current configuration
+  python3 train.py --num_nodes "${NUM_NODES}" --embedding_dimension "${EMBEDDING_DIMENSION}" --activation_key "${ACTIVATION_KEY}" --cpus_per_node 16
+
+  # Clearing previous train/test files to free disk space for next iteration
+  rm -rf ~/ray_results train_file* test_file*
+}
+
+# Run the script for specific configurations
+run_script 12 10240 "1B" 2>&1 | tee -a log_file.txt
+
